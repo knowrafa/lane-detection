01e8e4f9acdf215db841bbe315da740c0eab316f
Rafael Alessandro <knowrafa@pop-os.localdomain>
2020-06-28 23:20:42 -0300

    Adicionando operações morfológicas

--- image-processing/image-processing.py
+++ image-processing/image-processing.py
@@ -1,16 +1,32 @@
 import cv2
 import numpy as np
 
-s = 2
+s = 10
 
 def new_value(image, row, col):
-	temp = 0
+	new_intensity = 0
+	intensity = image[row,col]
 
 	for i in range(1, s+1, 1):
-		print(row, col, col-3*i,col+3*i, i)
-		temp = temp + min(max(image[row,col]- (image[row,col-(3*i if col-3*i >=0 else 0)]), 0), max(image[row,col]- (image[row,col+(3*i if col+3*i < width else 0)]), 0))
-		#print (temp, i)
-	return temp
+		#print(row, col, col-3*i,col+3*i, i)
+		#print(str(image[row][col]) + " -- " + str(temp))
+		#print (str(image[row,col-(3*i if (col-3*i >= 0) else 0)]) + " // " + str(image[row,col+(3*i if (col+3*i < width) else 0)]))
+		#print("MIN " + str(min(max(image[row,col]- (image[row,col-(3*i if (col-3*i >= 0) else 0)]), 0), max(image[row,col]- (image[row,col+(3*i if (col+3*i < width) else 0)]), 0))))
+		#print("FIRST CALC: " + str(image[row,col]- (image[row,col-(3*i if (col-3*i >= 0) else 0)])))
+		#print("SECOND CALC: " + str(image[row,col]- (image[row,col+(3*i if (col+3*i < width) else 0)])))
+
+		l3i = image[row,col-(3*i if (col-3*i >= 0) else 0)]
+		r3i = image[row,col+(3*i if (col+3*i < width) else 0)]
+		
+		#if(intensity < l3i):
+		#	l3i = 0
+
+		#if(intensity < r3i):
+		#	r3i = 0
+
+		new_intensity = new_intensity + min(max(int(intensity)- int(l3i) , 0), max(int(intensity)- int(r3i), 0))
+
+	return new_intensity
 
 
 lane_img = cv2.imread("lane-article.png") #ler imagem do disco
@@ -30,8 +46,13 @@ for r in range(height):
 cv2.imwrite("new_image.png", new_image)
 #cv2.imshow("teste", new_image)
 _, otsu_image  = cv2.threshold(new_image, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-print(otsu_image)
-#cv2.imwrite("new_otsu.png", otsu_image)
-cv2.imshow("OTSU", otsu_image)
+
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(4,4))
+
+closing = cv2.morphologyEx(otsu_image, cv2.MORPH_CLOSE, kernel)
+
+#print(otsu_image)
+cv2.imwrite("new_otsu.png", closing)
+cv2.imshow("OTSU", closing)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
